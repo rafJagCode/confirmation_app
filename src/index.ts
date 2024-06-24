d8eb0b294baa703bf25ea3d4d7e1e0d4472a684b
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-24 22:32:26 +0200

    Create basic express app

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,25 @@
 import express from 'express';
+import path from 'path';
+import { connectToDatabase } from './services/database.service';
+import companiesRouter from './routes/companies.router';
 
 const app = express();
 
-app.listen(8080, () => {
-  console.log('Server is runnin on port 8080.');
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+connectToDatabase()
+  .then(() => {
+    app.use('/resumes', companiesRouter);
+
+    app.use((req, res) => {
+      res.status(404).sendFile(path.join(__dirname, 'views/404.html'));
+    });
+
+    app.listen(8080, () => {
+      console.log('Server is runnin on port 8080.');
+    });
+  })
+  .catch((error: Error) => {
+    console.error('Database connection failed', error);
+    process.exit();
+  });
