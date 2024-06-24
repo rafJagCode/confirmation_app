95234337cd52d43c0536e64599a84af70a364bb7
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-24 02:05:53 +0200

    Adjust configuration

--- src/__tests__/sample.test.ts
+++ src/__tests__/sample.test.ts
@@ -3,12 +3,12 @@ import { add } from '../sample';
 const cases = [
   [1, 2, 3],
   [0, 0, 0],
-  [5, -10, -4],
+  [5, -10, -5],
 ];
 describe('Add function should return sum of two numbers', () => {
   test.each(cases)(
     `Sum of %p and %p should be equal to %p`,
-    (num1, num2, num3) => {
+    (num1: number, num2: number, num3: number) => {
       const result = add(num1, num2);
       expect(result).toEqual(num3);
     }
