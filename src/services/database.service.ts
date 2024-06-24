81e12f842c0bdcb751fbe736b341cc4da1ea3c29
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-24 22:29:24 +0200

    Add database service

---
+++ src/services/database.service.ts
@@ -0,0 +1,26 @@
+import * as mongoDB from 'mongodb';
+import dotenv from 'dotenv';
+
+export const collections: { companies?: mongoDB.Collection } = {};
+
+export async function connectToDatabase() {
+  dotenv.config();
+
+  let uri: string;
+  if (process.env.ENV === 'DEV') uri = process.env.DEV_DB_URI;
+  else uri = process.env.DB_URI;
+
+  const client: mongoDB.MongoClient = new mongoDB.MongoClient(uri);
+
+  await client.connect();
+
+  const db: mongoDB.Db = client.db('resumes');
+
+  const companiesCollection: mongoDB.Collection = db.collection('companies');
+
+  collections.companies = companiesCollection;
+
+  console.log(
+    `Successfully connected to database: ${db.databaseName} and collection: ${companiesCollection.collectionName}`
+  );
+}
