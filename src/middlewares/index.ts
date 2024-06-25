3e0db1b9b7afd621a4ada6171d7e21a96627dea4
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-25 22:44:02 +0200

    Export middlewares from one place

---
+++ src/middlewares/index.ts
@@ -0,0 +1,5 @@
+import authMiddleware from './authMiddleware';
+import errorMiddleware from './errorMiddleware';
+import notFoundMiddleware from './notFoundMiddleware';
+
+export { authMiddleware, errorMiddleware, notFoundMiddleware };
