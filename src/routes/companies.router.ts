5b8a9b2f2ee3ee58d3fe3888942d237155e53f54
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-25 22:45:24 +0200

    Refactor companies.router to use middlewares

--- src/routes/companies.router.ts
+++ src/routes/companies.router.ts
@@ -1,50 +1,55 @@
 import express from 'express';
 import path from 'path';
-import dotenv from 'dotenv';
 import { Company } from '../models/company';
 import { ObjectId } from 'mongodb';
 import { PushOperator } from 'mongodb';
 import { Document } from 'mongodb';
 import { collections } from '../services/database.service';
+import { authMiddleware } from '../middlewares/index';
 
 const router = express.Router();
 
-router.get('', async (req, res) => {
-  dotenv.config();
-  const apiKey = req.headers['api-key'];
-  if (apiKey === process.env.API_KEY) {
-    try {
-      const companies = await collections.companies.find({}).toArray();
-      res.status(200).send(companies);
-    } catch (err) {
-      res.status(500).send(err.message);
-    }
-  } else res.status(401).send({ error: 'Unauthorized' });
+router.get('', authMiddleware, async (req, res, next) => {
+  try {
+    const companies = await collections.companies.find({}).toArray();
+    res.status(200).send(companies);
+  } catch (err) {
+    next(err);
+  }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const id = req?.params?.id.trim();
     const query = { _id: new ObjectId(id) };
     const company = (await collections.companies.findOne(query)) as Company;
 
-    if (company) {
-      const $set = { visited: true };
-      const $push = {
-        visitedAt: new Date(),
-      } as unknown as PushOperator<Document>;
-      await collections.companies.updateOne(query, { $set, $push });
-    }
+    if (!company) return next();
+
+    const $set = { visited: true };
+    const $push = {
+      visitedAt: new Date(),
+    } as unknown as PushOperator<Document>;
+
+    await collections.companies.updateOne(query, { $set, $push });
 
     res.sendFile(path.join(__dirname, '../views/confirmation.html'));
   } catch (err) {
-    res.sendFile(path.join(__dirname, '../views/404.html'));
+    next(err);
   }
 });
 
-router.post('', async (req, res) => {
+router.post('', authMiddleware, async (req, res, next) => {
   try {
     const name = req.body.name;
+
+    if (!name || !name.trim().length) {
+      return next({
+        code: 400,
+        message: 'Name was not provided inside body request.',
+      });
+    }
+
     const company: Omit<Company, '_id'> = {
       name,
       visited: false,
@@ -53,15 +58,19 @@ router.post('', async (req, res) => {
 
     const result = await collections.companies.insertOne(company);
 
-    result
-      ? res
-          .status(201)
-          .send(
-            `Successfully created new company record with id ${result.insertedId}`
-          )
-      : res.status(500).send('Failed to create new company record.');
+    if (!result.acknowledged) {
+      return next({
+        code: 500,
+        message: 'Failed to create new company record.',
+      });
+    }
+
+    res.status(201).send({
+      message: 'Successfully created new company record.',
+      insertedId: result.insertedId,
+    });
   } catch (err) {
-    res.status(400).send(err.message);
+    next(err);
   }
 });
 
