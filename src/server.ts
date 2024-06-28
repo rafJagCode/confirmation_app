2490357d78fd3c036922e4f466ff77cdb9e88d32
Rafał Jagielski <jagielski.rafal.uwm@gmail.com>
2024-06-28 18:00:48 +0200

    Configure using vhost for subdomains

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,29 @@
+import express from 'express';
 import dotenv from 'dotenv';
+import vhost from 'vhost';
 import setupServer from './index';
 
 setupServer()
   .then((app) => {
+    let port: string;
+    let subdomain: null | string;
+
     dotenv.config();
-    const port =
-      process.env.ENV === 'PROD' ? process.env.PROD_PORT : process.env.DEV_PORT;
+    if (process.env.ENV === 'PROD') {
+      port = process.env.PROD_PORT;
+    } else {
+      port = process.env.DEV_PORT;
+    }
+
+    if (process.env.USE_SUBDOMAIN === 'true') {
+      subdomain = process.env.SUBDOMAIN;
+      app = express().use(vhost(subdomain, app));
+    }
+
     app.listen(port, () => {
-      console.log(`Server is runnin on port ${port}.`);
+      console.log(
+        `Server is runnin on port ${port}${subdomain ? ' using subdomain ' + subdomain : ''}.`
+      );
     });
   })
   .catch((err) => {
